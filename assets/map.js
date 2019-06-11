e06564b24d5639441c05c0682d2ea0cce784a364
Jonas Beinder <jonas@schmuhf.com>
2019-06-11 16:32:35 +0200

    display tracks on map and photos in sidebar

--- assets/map.js
+++ assets/map.js
@@ -39,8 +39,8 @@ function initMap() {
 
     map = L.map('map', {
         maxBounds: [
-            [-90, -180],
-            [90, 180]
+            [-90, -90],
+            [90, 270]
         ]
     }).setView([0, 0], 3);
 
@@ -58,6 +58,110 @@ function initMap() {
         "Color": color
     };
     L.control.layers(baseLayers, {}).addTo(map);
+
+    addGeoJSON();
+}
+
+let icon = L.divIcon({ className: 'circle'});
+
+let data = {path: {}, points: {}, images: {}};
+function addGeoJSON()
+{
+    let promises = [];
+    for(let rover of ['curiosity', 'spirit'])
+    {
+        $.ajax({
+            type: "GET",
+            url: "geodata/" + rover + "-path.geojson",
+            dataType: "json",
+            success: function (response) {
+                data.path[rover] = L.geoJson(response).addTo(map);
+                if(rover === 'curiosity')
+                    map.fitBounds(data.path[rover].getBounds())
+            }
+        });
+        $.ajax({
+            type: "GET",
+            url: "geodata/" + rover + "-points.geojson",
+            dataType: "json",
+            success: function (response) {
+                data.points[rover] = L.geoJson(response,
+                {
+                    onEachFeature: function(feature, layer) {
+                        feature.properties.rover = rover;
+                        layer.on({
+                            click: doclick
+                        });
+                    },
+                    pointToLayer: function (feature, latlng) {
+                        let tooltip = feature.properties.date_2 === null ? "Sol " + feature.properties.date_1 : "Sols " + feature.properties.date_1 + ' to ' + feature.properties.date_2;
+                        return L.marker(latlng, {icon: icon}).bindTooltip(tooltip);
+                    }
+                }).addTo(map);
+            }
+        });
+    }
+}
+
+function doclick(e)
+{
+    let p = e.target.feature.properties;
+
+    if(p.date_2 === null)
+        p.date_2 = p.date_1;
+
+    if(!data.images[p.rover])
+        data.images[p.rover] = {};
+
+    $(".sidebar").html("loading");
+
+    let promises = [];
+    for(let i = p.date_1; i <= p.date_2; i++)
+    {
+        promises.push(getImage(p.rover, i));
+    }
+
+    $.when.apply($, promises).done(function(){
+        let html = '';
+        for(let i = p.date_1; i <= p.date_2; i++)
+        {
+            html += '<h2>Sol ' + i + '</h2>';
+            html += '<h5>' + data.images[p.rover][i]['photos'].length + ' images</h5>';
+            let j;
+            for(j = 0; j < Math.min(24, data.images[p.rover][i]['photos'].length); j++)
+            {
+                html += '<img src="' + data.images[p.rover][i]['photos'][j].img_src + '" class="rover"/>';
+            }
+
+            if(j < data.images[p.rover][i]['photos'].length)
+            {
+                html += (data.images[p.rover][i]['photos'].length - j) + " images more";
+            }
+        }
+        $(".sidebar").html(html)
+    })
+}
+
+function getImage(rover, sol)
+{
+    let api_key = "iitToTP7Vq0aSzwdZwfDCcR4tNR5aMgA23KRZn0x";
+    let promise = $.Deferred();
+    if(!data.images[rover][sol])
+    {
+        $.ajax({
+            type: "GET",
+            url: "https://api.nasa.gov/mars-photos/api/v1/rovers/" + rover + "/photos?api_key=" + api_key + "&sol=" + sol,
+            dataType: "json"
+        }).done(function(response) {
+            data.images[rover][sol] = response;
+            promise.resolve();
+        });
+    }
+    else
+    {
+        promise.resolve();
+    }
+    return promise.promise();
 }
 
 /*
